8e15aa25f5315720f366fcfadfa597bbf9e681de
dannyvassallo <danielvassallo87@gmail.com>
2016-05-25 20:45:48 -0400

    yeah that was a mistake

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<?php
-
-  if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
-    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header('HTTP/1.1 301 Moved Permanently');
-    header('Location: ' . $redirect);
-    exit();
-  }
-
-  include_once("index.html");
-
-?>
+<?php include_once("index.html"); ?>
