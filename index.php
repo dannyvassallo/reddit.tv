f13d7f8cf0e8452a3c7ff8bbf1c6fa4e53b569d5
dannyvassallo <danielvassallo87@gmail.com>
2016-05-25 20:44:37 -0400

    testing redirect because php aint muh thang

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php include_once("index.html"); ?>
+<?php
+
+  if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
+    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $redirect);
+    exit();
+  }
+
+  include_once("index.html");
+
+?>
